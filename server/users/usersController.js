93eaf2aab18b62733ef195cfca94ffc7428ec21b
Scott Kao <scottkao85@gmail.com>
2015-03-25 11:29:41 -0700

    fix: able to sign in and save user name and pw in db

--- server/users/usersController.js
+++ server/users/usersController.js
@@ -5,6 +5,8 @@
 
 // The bcrypt module is used to salt and encrypt passwords
 var bcrypt = require('bcrypt');
+var User = require('./usersModel');
+var util = require('../config/helpers');
 // var session = require('express-session');
 
 module.exports = {
@@ -31,28 +33,29 @@ module.exports = {
 
   // Sign up a new user, and start their first session
   signup: function(req, res, next) {
+    console.log('at sign up');
     var username = req.body.username;
     var password = req.body.password;
-    console.log('request has:', username, password);
-    // (use below as model)
-    // // Bind the Mongoose create method to the Idea model, so that the Q module can use promises with it.
-    // var createIdea = Q.nbind(Idea.create, Idea);
 
-    // // Create a new document from the Idea model. If successfully created then the new Idea document is returned.
-    // var newIdea = {
-    //   title: req.body.title,
-    //   text: req.body.text
-    // };
-
-    // createIdea(newIdea)
-    //   .then(function (createdIdea) {
-    //     if (createdIdea) {
-    //         res.json(createdIdea);
-    //     }
-    //   })
-    //   .fail(function(error) {
-    //     next(error);
-    //   });
+    User.findOne({ username: username })
+    .exec(function(err, user) {
+      if (!user) {
+        console.log('new users')
+        var newUser = new User({
+          username: username,
+          password: password
+        });
+        newUser.save(function(err, newUser) {
+          if (err) {
+            res.send(500, err);
+          }
+          util.createSession(req, res, newUser);
+        });
+      } else {
+        console.log('Account already exists');
+        res.redirect('/#/signup');
+      }
+    });
   }
 
 };
--- server/users/usersModel.js
+++ server/users/usersModel.js
@@ -4,8 +4,11 @@
 // The User model defines the structure of all of the User documents created.
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var bluebird = require('bluebird');
 
-var UserSchema = new mongoose.Schema({
+
+var UserSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -21,4 +24,22 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('users', UserSchema);
+var User = mongoose.model('User', UserSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+UserSchema.pre('save', function(next){
+  var cipher = bluebird.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+module.exports = User;
